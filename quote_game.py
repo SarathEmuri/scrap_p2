0e58d1edd1f53e22b33235d8fb5316924c714764
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-20 13:53:44 -0500

    Add for loop code to get scrap each quote, author & url for bio from page 1 into a list

--- quote_game.py
+++ quote_game.py
@@ -1,7 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+all_quotes = []
 res = requests.get("http://quotes.toscrape.com")
 soup = BeautifulSoup(res.text, "html.parser")
 quotes = soup.find_all(class_="quote")
-print(quotes)
+for quote in quotes:
+    all_quotes.append({
+        "text": quote.find(class_="text").get_text(),
+        "author": quote.find(class_="author").get_text(),
+        "bio-link": quote.find("a")["href"]
+    })
+
+print(all_quotes)
