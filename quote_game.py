700fb0b8da5d6181407adba98b27574eae516ee4
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-21 11:23:42 -0500

    Modify code to extract quotes from all pages. Add sleep between pages

--- quote_game.py
+++ quote_game.py
@@ -1,15 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 
 all_quotes = []
-res = requests.get("http://quotes.toscrape.com")
-soup = BeautifulSoup(res.text, "html.parser")
-quotes = soup.find_all(class_="quote")
-for quote in quotes:
-    all_quotes.append({
-        "text": quote.find(class_="text").get_text(),
-        "author": quote.find(class_="author").get_text(),
-        "bio-link": quote.find("a")["href"]
-    })
+base_url= "http://quotes.toscrape.com"
+page_url= "/page/1"
+
+while page_url:
+    res = requests.get(f"{base_url}{page_url}")
+    print(f"Now scraping {page_url}....")
+    soup = BeautifulSoup(res.text, "html.parser")
+    quotes = soup.find_all(class_="quote")
+
+    for quote in quotes:
+        all_quotes.append({
+            "text": quote.find(class_="text").get_text(),
+            "author": quote.find(class_="author").get_text(),
+            "bio-link": quote.find("a")["href"]
+        })
+    next_btn = soup.find(class_="next")
+    page_url = next_btn.find("a")["href"] if next_btn else None
+    sleep(2)
 
 print(all_quotes)
